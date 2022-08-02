f8a7282a14029e35fee3feafb1a38ca0134f6176
chrisjm21 <110302568+chrisjm21@users.noreply.github.com>
2022-08-02 11:13:08 -0700

    Update speakhello.js

--- module4-sol/js/speakhello.js
+++ module4-sol/js/speakhello.js
@@ -1,17 +1,9 @@
-(function (window) {
-    // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
-  // See Lecture 52, part 2
-  
-  
-  var helloSpeaker = {};
-  
- 
-    var speakWord = "Hello";
-  
-    helloSpeaker.speak = function (name) {
-      console.log(speakWord + " " + name);
-    };
-  
-    window.helloSpeaker = helloSpeaker;
-  
-  })(window); 
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
