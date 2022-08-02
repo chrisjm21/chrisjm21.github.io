db1bc47de197f2fcc01013ae8c339fde64e5cdf3
chrisjm21 <110302568+chrisjm21@users.noreply.github.com>
2022-08-02 11:11:01 -0700

    Update script.js

--- module4-sol/js/script.js
+++ module4-sol/js/script.js
@@ -1,17 +1,16 @@
 (function () {
-   
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   
-    for (var i = 0; i < names.length; i++) {
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
   
-      
-       var firstLetter = names[i].charAt(0).toLowerCase();
-  
-      
-      if (firstLetter === 'j') {
-         byeSpeaker.speak(names[i]);
-      } else {
-        helloSpeaker.speak(names[i]);
-      }
+    if (firstLetter === 'j') {
+      byeSpeaker(names[i]);
     }
+    else {
+      helloSpeaker(names[i]);
+    }
+  }
+  
   })();
